f1d3425a88af8f4719d229e651f8e10d66b7ec5c
Evan Fadanelli <Evan Fadanelli@10.80.145.166>
2018-10-31 11:20:40 -0400

    Evan, Added an if statement

--- RepositoryPrac/src/RepMain.java
+++ RepositoryPrac/src/RepMain.java
@@ -1,10 +1,11 @@
 
 public class RepMain {
 
-	public static void main(String[] args) {
-		
+	public static void main(String[] args){
 		System.out.println("Partner 1");
 		System.out.println("DESPACITO");
+		boolean goingToCrash = false;
+		if(!goingToCrash)
+			System.out.print("Don't");
 	}
-
 }
